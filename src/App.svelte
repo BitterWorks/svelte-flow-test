5f710ad07abbbd47e948177687b9c828c71097ed
Lucas <lucasmiguelmac@gmail.com>
2022-01-30 00:40:08 +0000

    SPA form

--- src/App.svelte
+++ src/App.svelte
@@ -3,4 +3,8 @@
   import { routes } from '../.routify/routes';
 </script>
 
+<svelte:head>
+  <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
+</svelte:head>
+
 <Router {routes} />
