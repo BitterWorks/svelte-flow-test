f749ac632a143762251d781d7d62a36ad08c8817
Lucas <lucasmiguelmac@gmail.com>
2022-01-30 01:27:39 +0000

    Pages

---
+++ src/pages/about.svelte
@@ -0,0 +1,6 @@
+<main class="container">
+    <article>
+        <h1>¿Quienes Sómos?</h1>
+        Una manga de mogólicos que se hacen los que programan y lloran cuando no les sale algo. Buena gente.
+    </article>
+</main>
---
+++ src/pages/index.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+
+    import Announcement from "../components/index/Announcement.svelte";
+
+    interface Fruit {
+        name:   string,
+        color?: string,
+        amount: number,
+        id:     number
+    }
+
+    let fruits: Fruit[] = [
+        {name: "Manzana", color: "Red", amount: 5, id: 1},
+        {name: "Banana", color: "Yellow", amount: 1, id: 2},
+        {name: "Grape", color: "Purple", amount: 3, id: 3}
+    ];
+
+    function deleteFruit(id: number): void{
+        // Hacer acá el call a borrar la instancia de la fruta
+        fruits = fruits.filter((fruit) => fruit.id != id);
+        console.log(fruits);
+    };
+</script>
+<main class="container">
+
+    <Announcement />
+
+
+    {#each fruits as fruit (fruit.id)}
+        <article>
+            <h5>{fruit.name}</h5>
+            <p>Color: {fruit.color}</p>
+            <p>Cant.: {fruit.amount}</p>
+            <p>
+                Id: {fruit.id}
+                {#if fruit.amount < 4}
+                    <ins>
+                        ¡Quedan pocas!
+                    </ins>
+                {/if}
+            </p>
+            <button on:click={() => deleteFruit(fruit.id)}>Borrar Fruta</button>
+        </article>
+    {:else}
+        <p>Todavía no hay frutas</p>
+    {/each}
+
+</main>
+<style>
+    ins {
+        margin-left: 1rem;
+    }
+</style>
