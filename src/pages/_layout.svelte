45374f058e9d2bd718b13484b5636102e4baa3ec
Lucas <lucasmiguelmac@gmail.com>
2022-01-30 01:27:26 +0000

    Layout

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,2 +1,18 @@
+<script>
+    import {isActive, url, page, metatags} from '@roxi/routify';
+    
+    $: metatags.title = `Pomoly | ${$page.title}`;
+</script>
+
+<nav class="container-fluid">
+    <ul>
+    <li><strong>Prueba</strong></li>
+    </ul>
+    <ul>
+    <li><a class:secondary={!$isActive('./index')} href={$url('./index')}>Home</a></li>
+    <li><a class:secondary={!$isActive('./about')} href={$url('./about')}>About</a></li>
+    </ul>
+</nav>
+
 <slot>
 </slot>
